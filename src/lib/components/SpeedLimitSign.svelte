4be83d2fef7b70e2b2be75d8cffb7a60a9ec3f69
entrack-plataforma <entrack.plataforma@gmail.com>
2024-09-10 02:41:40 +0100

    add traffic sign

---
+++ src/lib/components/SpeedLimitSign.svelte
@@ -0,0 +1,4 @@
+<script>
+    export let limit
+</script>
+<img alt="{limit}" src="{`/signs/regulatory--maximum-speed-limit-${limit}--g1.svg`}" width="100%" />
--- src/routes/reports/speeding/+page.svelte
+++ src/routes/reports/speeding/+page.svelte
@@ -11,12 +11,13 @@
     import { t } from "$lib/i18n";
     import { FileChartBarSolid, FilePdfSolid} from "flowbite-svelte-icons";
     import { utils, writeFileXLSX } from 'xlsx';
+    import SpeedLimitSign from "$lib/components/SpeedLimitSign.svelte";
     export let data;
     let showExport = true
     let tbl
 </script>
 
-<svelte:window on:afterprint={showExport=true} />
+<svelte:window on:afterprint={() => showExport=true} />
 
 {#if showExport }
 <Toolbar embedded class="w-full">
@@ -43,51 +44,36 @@
 </Heading>
 
 <div bind:this={tbl}>
-<Table hoverable="true" >
+    <Table hoverable="true" class="table-fixed p-0">
     <TableHead class="border-y border-gray-200 bg-gray-100 dark:border-gray-700">
-        {#each [$t('time'), $t('address'), $t('max allowed'), $t('max detected'), $t('duration'), 'Duration', 'Actions'] as title}
-            <TableHeadCell class="text-center p-4 font-medium">{title}</TableHeadCell>
-        {/each}
+        <TableHeadCell class="text-center">{$t('time')}</TableHeadCell>
+        <TableHeadCell class="text-center">{$t('address')}</TableHeadCell>
+        <TableHeadCell class="text-center p-0 w-16">{$t('max allowed')}</TableHeadCell>
+        <TableHeadCell class="text-center p-0 w-20">{$t('max detected')}</TableHeadCell>
+        <TableHeadCell class="text-center">{$t('duration')}</TableHeadCell>
+        <TableHeadCell class="text-right w-16">{$t('km')}</TableHeadCell>
     </TableHead>
     <TableBody>
         {#each data.events as event}
-            <TableBodyRow class="text-base">
-                <TableBodyCell class="p-4">{new Date(event.fixTime).toLocaleString()}</TableBodyCell>
-                <TableBodyCell class="text-center max-w-sm overflow-hidden truncate p-4 text-base font-normal text-gray-500 dark:text-gray-400 xl:max-w-xs">
-                    {(event.names && event.names.join(',')) || ''}
+            <TableBodyRow class="text-xs">
+                <TableBodyCell class="text-center overflow-hidden overflow-ellipsis p-0">
+                    {new Date(event.fixTime).toLocaleString()}
                 </TableBodyCell>
-
-                <TableBodyCell class="text-center max-w-sm overflow-hidden truncate p-4 text-base font-normal text-gray-500 dark:text-gray-400 xl:max-w-xs">
-                    {event.speed_limit}
+                <TableBodyCell class="p-0 text-center overflow-hidden overflow-ellipsis">
+                    <a target="_blank" href="https://openstreetmap.org/way/{event.way_id}">{(event.names && event.names.join(',')) || ''}</a>
                 </TableBodyCell>
-                <TableBodyCell class="text-center max-w-sm overflow-hidden truncate p-4 text-base font-normal text-gray-500 dark:text-gray-400 xl:max-w-xs">
+                <TableBodyCell>
+                    <SpeedLimitSign limit="{event.speed_limit}"/>
+                </TableBodyCell>
+                <TableBodyCell class="text-right">
                     {Math.round(event.maxSpeed * 1.852)}
                 </TableBodyCell>
-                <TableBodyCell class="text-center max-w-sm overflow-hidden truncate p-4 text-base font-normal text-gray-500 dark:text-gray-400 xl:max-w-xs">
+                <TableBodyCell class="text-center">
                     {event.points.length > 1? new Date(event.eventTime).toISOString().substring(11, 19) : ''}
                 </TableBodyCell>
-                <TableBodyCell class="text-center max-w-sm overflow-hidden truncate p-4 text-base font-normal text-gray-500 dark:text-gray-400 xl:max-w-xs">
-                    dist: {event.distance}
+                <TableBodyCell class="text-right">
+                    {event.points.length > 1 ? Math.round(event.distance) : ''}
                 </TableBodyCell>
-                <TableBodyCell class="text-center max-w-sm overflow-hidden truncate p-4 text-base font-normal text-gray-500 dark:text-gray-400 xl:max-w-xs">
-                    points: {event.points.length}
-                </TableBodyCell>
-                <TableBodyCell class="text-center max-w-sm overflow-hidden truncate p-4 text-base font-normal text-gray-500 dark:text-gray-400 xl:max-w-xs">
-                    {event.way_id}
-                </TableBodyCell>
-
-
-                <TableBodyCell class="flex items-center space-x-6 whitespace-nowrap p-4  overflow-hidden truncate">
-                    <div class="text-sm font-normal text-gray-500 dark:text-gray-400">
-                        <div class="text-base font-semibold text-gray-900 dark:text-white">{event.speed_limit}</div>
-                        <div class="text-sm font-normal text-gray-500 dark:text-gray-400">asd</div>
-                    </div>
-                </TableBodyCell>
-
-                <TableBodyCell class="text-center max-w-sm overflow-hidden truncate p-4 text-base font-normal text-red-500 dark:text-gray-400 xl:max-w-xs">
-                    {event.model}
-                </TableBodyCell>
-
             </TableBodyRow>
         {/each}
     </TableBody>
@@ -96,7 +82,10 @@
 
 <style>
     @page {
-        size: A4;
+        size: A4 landscape;
         margin: 5mm;
+    }
+    div {
+        print-color-adjust: exact;
     }
 </style>
