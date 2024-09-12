5b18e93e7560eb879faf0975fed680ea7898ba57
entrack-plataforma <entrack.plataforma@gmail.com>
2024-09-12 02:23:41 +0100

    https

--- src/routes/reports/speeding/+page.server.js
+++ src/routes/reports/speeding/+page.server.js
@@ -16,7 +16,7 @@ async function getEvents(selected, traccar, searchParams, request) {
 
 export async function load({request, platform}) {
     const traccar = (platform && platform.env.TRACCAR_SERVER) || import.meta.env.VITE_TRACCAR_SERVER
-    const s = (platform && platform.env.TRACCAR_SERVER_HTTPS) || ''
+    const s = platform && platform.env.TRACCAR_SERVER_HTTPS ? 's' : ''
     const {searchParams} = new URL(request.url)
     const selected = searchParams.get('selected').split(',')
     return {events: await getEvents(selected, `http${s}://${traccar}`, searchParams, request)}
