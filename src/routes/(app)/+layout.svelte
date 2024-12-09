0f94ed35b023e14b18a7776c6b3624ac3fb2a336
joaquimcardeira <joaquim.cardeira@gmail.com>
2024-12-09 22:37:27 +0000

    remove navbar

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -6,10 +6,6 @@
 
 </script>
 
-<header class="fixed top-0 z-40 mx-auto w-full flex-none border-b border-gray-200 bg-white dark:border-gray-600 dark:bg-gray-800">
-    <Navbar bind:drawerHidden />
-</header>
-
 <div class="flex min-h-full">
     <Sidebar bind:drawerHidden />
     <div class="min-h-full grow overflow-y-auto pt-[60px]">
