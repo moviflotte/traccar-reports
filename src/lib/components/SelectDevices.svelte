6a47b9585fea5df178be8bef012f4e267109bca4
joaquimcardeira <joaquim.cardeira@gmail.com>
2025-01-30 11:22:14 +0000

    select devices

--- src/lib/components/SelectDevices.svelte
+++ src/lib/components/SelectDevices.svelte
@@ -2,9 +2,10 @@
     import { MultiSelect } from 'flowbite-svelte';
     export let selected;
     export let devices;
+    import {t} from '$lib/i18n'
 </script>
 
-<MultiSelect placeholder="Select devices..." items={
+<MultiSelect placeholder="{t('Select devices')}..." items={
     devices.sort((a, b) => a.name.localeCompare(b.name)).map(d => ({value: d.id, ...d}))
     } bind:value={selected} size="lg"
 />
--- src/lib/translations.js
+++ src/lib/translations.js
@@ -3,12 +3,14 @@ export default {
         "time": "Hora",
         "Positions": "Posiciones",
         "Speeding": "Velocidade",
-        "Position report": "Relatório de posições"
+        "Position report": "Relatório de posições",
+        "Select devices": "Seleccionar veículo"
     },
     es: {
         "time": "Hora",
         "Positions": "Posiciones",
         "Speeding": "Excesos de velocidad",
-        "Position report": "Reporte de posiciones"
+        "Position report": "Reporte de posiciones",
+        "Select devices": "Seleccione vehículo"
     },
 };
