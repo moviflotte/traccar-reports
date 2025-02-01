1ec164bf0385af7d296a770d2dac45994b04f130
jcardus <asklocation.net@gmail.com>
2025-02-01 21:23:33 +0000

    "TypeError: Cannot read properties of undefined (reading 'substring')"

--- src/lib/i18n.js
+++ src/lib/i18n.js
@@ -1,6 +1,6 @@
 import translations from "./translations";
 
-let locale = typeof navigator !== "undefined" && navigator.language.substring(0, 2)
+let locale = typeof navigator !== "undefined" && navigator.language && navigator.language.substring(0, 2)
 
 export const t = ( key ) => (translations[locale] && translations[locale][key]) || key
 
